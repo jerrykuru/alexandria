2ea5ce1d81c71900d137bef35df0e9ccf3ee4e42
Jerry Kuru <jerrykuru@gmail.com>
2016-02-10 14:16:36 -0500

    Removed Debug comments

--- app/src/main/java/it/jaschke/alexandria/Utility.java
+++ app/src/main/java/it/jaschke/alexandria/Utility.java
@@ -3,7 +3,6 @@ package it.jaschke.alexandria;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.util.Log;
 
 /**
  * Created by binu on 2/10/16.
@@ -14,9 +13,6 @@ public  class Utility {
         ConnectivityManager connectivityManager =  (ConnectivityManager) c.getSystemService(c.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo =  connectivityManager.getActiveNetworkInfo();
         boolean hasConnection = networkInfo != null && networkInfo.isConnectedOrConnecting();
-        Log.v(TAG, "JERRY -----");
-        Log.v(TAG,hasConnection + "");
-        Log.v(TAG,"JERRY -----");
         return hasConnection;
     }
 }
